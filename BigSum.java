1578d0c8f5eb00d1998131e059fb2589aa232ca0
abhishek15725 <54860064+abhishek15725@users.noreply.github.com>
2021-04-11 08:27:19 +0530

    Create BigSum.java
    Long Sum done!

---
+++ BigSum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class BigSum {
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n);
+        long[] ar=new long[n];
+        long sum = 0;
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextLong();
+            sum = sum + ar[i];
+        }
+        System.out.println(sum);
+    }
+}
